7c4b2993d4ef0db01372cf246fad3f185cc75cd3
seangnewman <seangnewman@msn.com>
2021-01-16 12:06:21 -0700

    Introducing the Task

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using StockAnalyzer.Core.Domain;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -25,17 +27,12 @@ namespace StockAnalyzer.Windows
 
 
 
-        private async void Search_Click(object sender, RoutedEventArgs e)
+        //private async void Search_Click(object sender, RoutedEventArgs e)
+        private  void Search_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
 
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+            
+             
 
             //BeforeLoadingStockData();
             #region asynchronous call must be replaced with asynchronous HttpClient
@@ -84,10 +81,38 @@ namespace StockAnalyzer.Windows
             await getStocksTask;
             #endregion
             #region catching exceptions
+            //try
+            //{
+            //    BeforeLoadingStockData();
+            //    await GetStocks();   // validate no exceptions in async operations
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Notes.Text = ex.Message;
+            //}
+            //finally
+            //{
+            //    AfterLoadingStockData();
+            //}
+
+            #endregion
+
+            #region Introducing Tasks
+
             try
             {
                 BeforeLoadingStockData();
-                await GetStocks();   // validate no exceptions in async operations
+
+                var lines = File.ReadAllLines("StockPrices_Small.csv");
+                var data = new List<StockPrice>();
+                foreach (var line in lines.Skip(1))
+                {
+                    var price = StockPrice.FromCSV(line);
+                    data.Add(price);
+                }
+
+                Stocks.ItemsSource = data.Where(sp => sp.Identifier == StockIdentifier.Text);
             }
             catch (Exception ex)
             {
@@ -99,7 +124,7 @@ namespace StockAnalyzer.Windows
                 AfterLoadingStockData();
             }
 
-            #endregion  
+            #endregion
             //AfterLoadingStockData();
         }
 
